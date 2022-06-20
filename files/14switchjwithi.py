38df8df1fb05dea4b3912bb48d5b26ca0ce13bee
Inferno0990 <sauravsuresh171@gmail.com>
2022-06-20 21:40:26 +0530

    Completed map

--- files/14switchjwithi.py
+++ files/14switchjwithi.py
@@ -1,16 +1,14 @@
 def replace(sample):
-    with open(sample,'r') as f:
-        a=f.read().split()
+    with open(sample, 'r') as f:
+        a = f.read().split()
         for i in a:
-            length=len(i)
-            for j in range(0,length):
-                if i[j]!='i':
+            length = len(i)
+            for j in range(0, length):
+                if i[j] != 'i':
                     print(i[j])
-                elif i[j]=='i':
-                    b=i[j].replace('i','j')
+                elif i[j] == 'i':
+                    b = i[j].replace('i', 'j')
                     print(b)
-
-
 
 
 replace("sample.txt")
--- files/17wordFrequency.py
+++ files/17wordFrequency.py
@@ -1,11 +1,13 @@
 def frequency(sample):
-    with open(sample,'r') as f:
-        word=dict()
-        a=f.read().split()
+    with open(sample, 'r') as f:
+        word = dict()
+        a = f.read().split()
         for i in a:
             if i in word:
-                word[i]+=1
+                word[i] += 1
             else:
-                word[i]=1
+                word[i] = 1
     print(word)
+
+
 frequency("sample.txt")
