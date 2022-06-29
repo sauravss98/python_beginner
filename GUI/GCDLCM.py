3f153534f4c122f8ac7ac5f6a25e9e0f7932c877
Inferno0990 <sauravsuresh171@gmail.com>
2022-06-29 11:10:36 +0530

    Gui completed

--- GUI/GCDLCM.py
+++ GUI/GCDLCM.py
@@ -1,8 +1,6 @@
 from  tkinter import  *
 from tkinter.ttk import *
-def GCD():
-    m=int(val1.get())
-    n=int(val2.get())
+def GCD(m,n):
     if m > n:
         s = n
     else:
@@ -10,10 +8,10 @@ def GCD():
     for i in range(1, s + 1):
         if ((m % i == 0) and (n % i == 0)):
             hcf = i
-    ans['text'] = "HCF is " + str(hcf)
-def LCM():
-    m = int(val1.get())
-    n = int(val2.get())
+    return hcf
+
+
+def LCM(m,n):
     if m>n:
         g=m
     else:
@@ -23,7 +21,16 @@ def LCM():
             lcm=g
             break
         g+=1
-    ans['text']="LCM is "+str(lcm)
+    return lcm
+
+def callFunction(event):
+    select=combo.get()
+    m=int(val1.get())
+    n=int(val2.get())
+    if select=="GCD":
+        ans['text'] = "HCF is " + str(GCD(m,n))
+    else:
+        ans['text'] = "LCM is " + str(LCM(m,n))
 
 root = Tk()
 root.title("LCM-GCD")
@@ -35,10 +42,8 @@ val1 = Entry(root)
 val2=Entry(root)
 btn = Button(root, text="Click", )
 ans = Label(root)
-combo=Combobox(root)
-combo['values']=["GCD","LCM"]
-combo.current()
-
+combo=Combobox(root,values=["GCD","LCM"])
+combo.bind("<<ComboboxSelected>>",callFunction)
 combo.grid(row=2,column=1)
 label1.grid(row=0, column=0)
 label2.grid(row=1,column=0)
